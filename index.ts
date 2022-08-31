dcc3e3f83dead399978e70d104ea53394ebf10a8
Fevronia Gjini <fevronigjini1@gmail.com>
2022-09-01 00:23:14 +0200

    id done

--- index.ts
+++ index.ts
@@ -9,38 +9,53 @@ const port = 5000
 const quotes =[
     {
         id:1,
-        author: "Winston Churchill",
-        quote:"Success is not final, failure is not fatal: it is the courage to continue that counts."
+        firstName: "Winston",
+        lastName:"Churchill",
+        quote:"Success is not final, failure is not fatal: it is the courage to continue that counts.",
+        image:"https://www.normandy1944.info/templates/yootheme/cache/d-day-normandy-beyond-commanders-churchill-936c0daf.jpeg"
         
     },
     {
         id:2,
-        author: "Oprah Winfrey",
-        quote:"You define your own life. Don't let other people write your script."
+        firstName: "Oprah",
+        lastName: "Winfrey",
+        age:68,
+        quote:"You define your own life. Don't let other people write your script.",
+        image:"https://www.biography.com/.image/t_share/MTY2NTIzMDQzOTIzODk1NTM4/oprah-photo-by-vera-anderson_wireimage.jpg"
         
     },
     {
         id:3,
-        author: "Walt Whitman",
-        quote:"Keep your face always toward the sunshine, and shadows will fall behind you"
+        firstName: "Walt",
+        lastname:" Whitman",
+        quote:"Keep your face always toward the sunshine, and shadows will fall behind you",
+        image:"https://www.biography.com/.image/t_share/MTE5NTU2MzE2NDA0ODExMjc1/walt-whitman-9530126-1-402.jpg"
         
     },
     {
         id:4,
-        author: "Audrey Hepburn",
-        quote:"Nothing is impossible. The word itself says 'I'm possible!"
+        firstName: "Audrey",
+        lastName:"Hepburn",
+        quote:"Nothing is impossible. The word itself says 'I'm possible!",
+        image:"https://www.biography.com/.image/t_share/MTc5OTk1NTMxMTYwODU1ODk2/audrey-hepburn-gettyimages-517443052.jpg"
         
     },
     {
         id:5,
-        author: " Lady Gaga",
-        quote:"Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on."
+        firstName: " Lady ",
+        lastName:"Gaga",
+        age:36,
+        quote:"Do not allow people to dim your shine because they are blinded. Tell them to put some sunglasses on.",
+        image:"https://www.ngnews247.com/wp-content/uploads/2021/05/Lady-Gaga-Biography.jpg"
         
     },
     {
         id:6,
-        author: "Mother Tereza",
-        quote:"Spread love everywhere you go."
+        firstName: "Ken",
+        lastName:"Poirot",
+        age:50,
+        quote:"Today is your opportunity to build the tomorrow that you want.",
+        image:"https://i.pinimg.com/736x/20/4c/26/204c26eb24ece5255d495d3e1f3788b4.jpg"
         
     }
 
@@ -59,9 +74,19 @@ app.get('/',(req, res) =>{
     `)
 })
 
+    app.get('/quotes/:id', (req, res)=>{
+        const id = Number(req.params.id)
+        const idMatch = quotes.find(quote => quote.id === id)
+        res.send(idMatch)
+    })
+     
+    app.get
+    
     app.get('/quotes', (req, res)=>{
         res.send(quotes)
     })
+    
+
 
     app.listen(port,()=>{
      console.log(` Yessss!!! http://localhost:${port} `)
